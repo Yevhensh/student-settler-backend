22b3b2c29bfb4acb61432b09f9ea586143cc50b8
yevhenshabala <yevhen.shabala@gmail.com>
2019-12-14 11:09:11 +0200

    SS-28 Added finding rooms by dormitoryId

--- src/main/java/edu/students/settler/controller/dormitory/impl/DormitoryControllerImpl.java
+++ src/main/java/edu/students/settler/controller/dormitory/impl/DormitoryControllerImpl.java
@@ -2,14 +2,17 @@ package edu.students.settler.controller.dormitory.impl;
 
 import edu.students.settler.controller.dormitory.DormitoryController;
 import edu.students.settler.model.entity.Dormitory;
+import edu.students.settler.model.entity.Room;
 import edu.students.settler.service.DormitoryService;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Set;
 
 @RestController
 @RequestMapping("/dormitories")
@@ -22,5 +25,11 @@ class DormitoryControllerImpl implements DormitoryController {
     @GetMapping
     public List<Dormitory> findAll() {
         return dormitoryService.findAll();
+    }
+
+    @Override
+    @RequestMapping("/{dormitoryId}/rooms")
+    public Set<Room> getDormitoryRooms(@PathVariable long dormitoryId) {
+        return dormitoryService.findDormitoryRooms(dormitoryId);
     }
 }
--- src/main/java/edu/students/settler/service/impl/DormitoryServiceImpl.java
+++ src/main/java/edu/students/settler/service/impl/DormitoryServiceImpl.java
@@ -1,13 +1,16 @@
 package edu.students.settler.service.impl;
 
 import edu.students.settler.model.entity.Dormitory;
+import edu.students.settler.model.entity.Room;
 import edu.students.settler.model.repository.DormitoryRepository;
 import edu.students.settler.service.DormitoryService;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.Set;
 
 @Component
 @AllArgsConstructor(onConstructor_ = @Autowired)
@@ -18,5 +21,10 @@ class DormitoryServiceImpl implements DormitoryService {
     @Override
     public List<Dormitory> findAll() {
         return dormitoryRepository.findAll();
+    }
+
+    @Override
+    public Set<Room> findDormitoryRooms(long dormitoryId) {
+        return dormitoryRepository.findById(dormitoryId).map(Dormitory::getRooms).orElse(Collections.emptySet());
     }
 }
